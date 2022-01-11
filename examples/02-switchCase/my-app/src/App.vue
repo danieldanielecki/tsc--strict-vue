4e58d20d53638a9edeb6d7eac8d2c95a1b0b9726
Daniel Danielecki <daniel.danielecki@mail.com>
2022-01-11 20:35:19 +0100

    Add: 02-switchCase example

--- examples/02-switchCase/my-app/src/App.vue
+++ examples/02-switchCase/my-app/src/App.vue
@@ -1,20 +1,47 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <p>Today is:</p>
+    <p>{{ today }}</p>
+    <button @click="myFunction()">Which day</button>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from './components/HelloWorld.vue';
+import { Component, Vue } from "vue-property-decorator";
 
-@Component({
-  components: {
-    HelloWorld,
-  },
-})
-export default class App extends Vue {}
+@Component({})
+export default class App extends Vue {
+  public today: Date = new Date();
+
+  myFunction(): void {
+    switch (this.today.getDay()) {
+      case 0:
+        alert("Sunday");
+        break;
+      case 1:
+        alert("Monday");
+        break;
+      case 2:
+        alert("Tuesday");
+        break;
+      case 3:
+        alert("Wednesday");
+        break;
+      case 4:
+        alert("Thursday");
+        break;
+      case 5:
+        alert("Friday");
+        break;
+      case 6:
+        alert("Saturday");
+        break;
+      default:
+        alert("No information available for that day.");
+        break;
+    }
+  }
+}
 </script>
 
 <style>
